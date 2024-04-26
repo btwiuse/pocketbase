d7d6c3642bb68247c9bcb039d0e0a292894ed420
navigaid <navigaid@gmail.com>
2024-04-26 17:24:25 +0000

    refactor: simplify diff wrt master

--- apis/envs.go
+++ apis/envs.go
@@ -1,6 +1,11 @@
 package apis
 
-import "github.com/webteleport/utils"
+import (
+	"crypto/tls"
+	"net/http"
+
+	"github.com/webteleport/utils"
+)
 
 var (
 	HOST       = utils.EnvHost("localhost")
@@ -12,3 +17,45 @@ var (
 	HTTPS_PORT = utils.LookupEnvPort("HTTPS_PORT")
 	ALT_SVC    = utils.EnvAltSvc("")
 )
+
+func LocalTLSConfig(certFile, keyFile string) *tls.Config {
+	GetCertificate := func(*tls.ClientHelloInfo) (*tls.Certificate, error) {
+		// Always get latest localhost.crt and localhost.key
+		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			return nil, err
+		}
+		return &cert, nil
+	}
+	return &tls.Config{
+		GetCertificate: GetCertificate,
+		NextProtos:     []string{"http/1.1"},
+	}
+}
+
+func ListenAndServeTLS(router http.Handler) error {
+	tlsConfig := LocalTLSConfig(CERT, KEY)
+	ln, err := tls.Listen("tcp4", *HTTPS_PORT, tlsConfig)
+	if err != nil {
+		println(err.Error())
+		return err
+	}
+	err = http.Serve(ln, router)
+	if err != nil {
+		println(err.Error())
+		return err
+	}
+	return nil
+}
+
+func ListenAndServe(router http.Handler) error {
+	if HTTPS_PORT != nil {
+		go ListenAndServeTLS(router)
+	}
+	err := http.ListenAndServe(PORT, router)
+	if err != nil {
+		println(err.Error())
+		return err
+	}
+	return nil
+}
