f17d465bde99607b100c25d5050a20fc0242267a
Broken Pipe <54848194+btwiuse@users.noreply.github.com>
2024-12-08 06:42:36 +0800

    squash rebase pocketbase master (#2)
    * squash and merge (#1)
    
    * embed relay server
    
    * ui: display data.host
    
    * disable http2 && fix connect proxy request logging
    
    * remove println
    
    * remove custom error handler
    
    * drop /_/ ui
    
    * restore logRequest
    
    * refactor: simplify diff wrt master
    
    * rename executable to pocket
    
    * add POCKETBASE_UI gate
    
    * add notes about http2 disablement
    
    ---------
    
    Co-authored-by: navigaid <navigaid@gmail.com>
    
    * bump deps
    
    * goreleaser: add pocket
    
    * pocket add Run func
    
    * github actions: disable ui npm build && go test
    
    * test release
    
    * add update cmd
    
    * fix comparison logic: always upgrade from `(uptracked)`
    
    * disable draft release
    ---------
    
    Co-authored-by: navigaid <navigaid@gmail.com>

---
+++ examples/pocket/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/apis"
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/plugins/ghupdate"
+	"github.com/webteleport/relay"
+)
+
+func main() {
+	if err := Run(os.Args[1:]); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Run(args []string) error {
+	app := pocketbase.New()
+
+	app.RootCmd.ParseFlags(args)
+
+	// GitHub selfupdate
+	ghupdate.MustRegister(app, app.RootCmd, ghupdate.Config{
+		Owner: "btwiuse",
+		Repo: "pocketbase",
+		ArchiveExecutable: "pocket",
+	})
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		app.Logger().Info("starting the relay server", "HOST", apis.HOST)
+		store := relay.NewSessionStore()
+		mini := relay.NewWSServer(apis.HOST, store)
+
+		withRelay := func(next http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				isPocketbaseHost := mini.IsRootExternal(r)
+				isPocketbaseAPI := strings.HasPrefix(r.URL.Path, "/api/")
+				isPocketbase := isPocketbaseHost && isPocketbaseAPI
+
+				if os.Getenv("POCKETBASE_UI") != "" {
+					isPocketbaseUI := strings.HasPrefix(r.URL.Path, "/_/")
+					isPocketbase = isPocketbaseHost && (isPocketbaseAPI || isPocketbaseUI)
+				}
+
+				// route non pocketbase requests to relay
+				if !isPocketbase {
+					mini.ServeHTTP(w, r)
+					return
+				}
+
+				next.ServeHTTP(w, r)
+			})
+		}
+
+		e.Router.Pre(
+			echo.WrapMiddleware(withRelay),
+		)
+		return nil
+	})
+
+	return app.Start()
+}
